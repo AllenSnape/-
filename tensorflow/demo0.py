4aa2275a4fda8f014e33823f5b7e5c41cbf3923e
AllenSnape <20764837+AllenSnape@users.noreply.github.com>
2018-05-16 13:09:18 +0800

    添加使用GPU参数, 添加label的显示

--- tensorflow/demo0.py
+++ tensorflow/demo0.py
@@ -7,7 +7,8 @@ options = {
     "model": "cfg/yolo.cfg", 
     # https://pjreddie.com/media/files/yolo.weights
     "load": "/home/as/Documents/AI/ML/darkflow/bin/yolo.weights", 
-    "threshold": 0.1
+    "threshold": 0.1,
+	"gpu":	1.0
 }
 # 载入参数, 初始化变量
 tfnet = TFNet(options)
@@ -23,10 +24,16 @@ result.sort(key=lambda x: x['confidence'], reverse=True)
 # 输出预测内容
 #print(result)
 
+# 线条颜色
 black = (0, 0, 0)
+# 标签字体
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 for obj in result:
     if obj['confidence'] > 0.5:
+        # 添加标签
+        imgcv = cv2.putText(imgcv, obj['label'], (obj['topleft']['x']+8, obj['topleft']['y']+20), font, 0.5, (255, 255, 255))
+        # 绘制线条
         cv2.line(imgcv, (obj['topleft']['x'], obj['topleft']['y']), (obj['bottomright']['x'], obj['topleft']['y']), black, 3)
         cv2.line(imgcv, (obj['topleft']['x'], obj['topleft']['y']), (obj['topleft']['x'], obj['bottomright']['y']), black, 3)
         cv2.line(imgcv, (obj['bottomright']['x'], obj['bottomright']['y']), (obj['bottomright']['x'], obj['topleft']['y']), black, 3)
